eb742c46a49b51aa5f95795a502ac129e25da904
K-138 <124754638+K-138@users.noreply.github.com>
2023-03-10 17:44:21 +0200

    Add files via upload

---
+++ student_8310_script.js
@@ -0,0 +1,95 @@
+let seats = document.getElementsByClassName("seat");
+let container = document.querySelector('.container');
+let btn= document.getElementById('btn')
+let pass=document.getElementById("pass");
+
+let sub = document.getElementById('sub');
+let seatnum=document.getElementById('seatnum');
+let name;
+var socket = io();
+
+//initializing variables
+let total=0;
+let indices=[0];
+let result=[0];
+btn.disabled=true;
+function sumArray(array) 
+{
+  let sum = 0;
+  for (let i = 0; i < array.length; i += 1) 
+  {
+    sum +=array[i];
+  }
+  return sum;
+}
+
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat")
+  ) {
+    e.target.classList.toggle("selected");
+  }
+
+  for(i=0;i<120;i++)
+  {
+    if(seats[i].classList.value=="seat selected")
+    {indices[i]=1;
+    name=seats[i].id;}
+    else
+    {
+      indices[i]=0;
+    }
+  
+  }
+
+  total=sumArray(indices);
+
+  if(total != 1)
+  {
+    btn.disabled=true;
+  }
+  else
+  {
+    btn.disabled=false;
+  }
+
+//click here
+  btn.onclick=function()
+  {
+    btn.style.display="none";
+    sub.style.display="inline";
+     seatnum.value=name;
+
+    socket.emit('addseat_8310', indices.indexOf(1));
+
+
+    // fetch('/getData_8310', {method: 'GET'})
+    // .then((response) => {
+    //   return response.json()
+    // })
+    // .then((data) => {
+    //   console.log(data)
+    //   result = data;
+    //   for (let i = 0; i < result.length; i += 1) 
+    //   {
+    //     result[i]+=indices[i];
+    //   }
+
+    //   return fetch('/sendData_8310', {method: 'POST', body: JSON.stringify(result), headers: {"Content-Type": "application/json"}})
+      
+    // })
+    // .then((response) => {
+    //   console.log(response);
+    //   //window.location.href="https://www.google.com/";
+    //   btn.style.display="none";
+    //   sub.style.display="inline";
+    //   seatnum.value=name;
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // })
+
+  }
+});
+
+
---
+++ teacher_8208_script.js
@@ -0,0 +1,95 @@
+let seats = document.getElementsByClassName("seat");
+let container = document.querySelector('.container');
+// let btn2=document.getElementById('btn2');
+let btn3=document.getElementById('btn3');
+let btn4=document.getElementById('btn4');
+
+//initializing variables
+let result=[];
+//start
+
+let socket = io()
+
+socket.emit('iamteacher_8208', 'kjdfbkd');
+
+socket.on ('sendData_8208', (data) => {
+  result = JSON.parse(data);
+  for(i=0;i<result.length;i++)
+  if(result[i]>1)
+  {
+    seats[i].className="seat cheating";
+  }
+  else if(result[i]==1)
+  {
+    seats[i].className="seat selected";
+  }
+  else if(result[i]==0)
+  {
+    seats[i].className="seat unselected";
+  }
+})
+
+window.onload = () => {
+  socket.emit('iamteacher_8208', 'kjdfbkd')
+}
+
+btn3.onclick=function()
+{
+    console.log("helloworld");
+    for(i=0;i<120;i++)
+    {
+      result[i]=0;
+    }
+
+    
+    fetch('/sendData_8208_teacher', {method: 'POST', body: JSON.stringify(result), headers: {"Content-Type": "application/json"}})
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    window.location.reload();
+}
+
+  // btn2.onclick=function()
+  // {
+  //   localStorage.clear();
+  // };
+
+//show
+//  btn4.onclick=function()
+//  {
+
+//   fetch('/getData_8208_teacher', {method: 'GET'})
+//   .then((response) => {
+//     return response.json()
+//   })
+//   .then((data) => {
+//     console.log(data) 
+//     result=data;
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+
+
+//   console.log(result);
+//   for(i=0;i<result.length;i++)
+//   if(result[i]>1)
+//   {
+//     seats[i].className="seat cheating";
+//   }
+//   else if(result[i]==1)
+//   {
+//     seats[i].className="seat selected";
+//   }
+//   else if(result[i]==0)
+//   {
+//     seats[i].className="seat unselected";
+//   }
+  
+//  }
+
+
+
